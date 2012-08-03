13512e5824babed414763532b00deeb809fe1df3
Rob Matthews <barristerbooks@gmail.com>
2012-08-03 18:44:26 +0100

    Finished Microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,10 @@ class User < ActiveRecord::Base
 		encrypted_password == encrypt(submitted_password)
 	end
 
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
+
 	def User.authenticate(email, submitted_password)
 		user = find_by_email(email)
 		(user && user.has_password?(submitted_password)) ? user : nil 
